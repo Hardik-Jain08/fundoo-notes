dcc40bd136ef7ba1b52094eca37ca2e90e7875d0
Hardik Jain <Hardik.Jain@godigit.com>
2024-08-20 18:24:02 +0530

    feat/authguard-and-getnotesapi

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from 'src/app/services/notes-service/notes.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,53 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private notesService: NotesService) { }
 
+  
+  notes = []
   ngOnInit(): void {
+    this.notesService.getAllNotes('getNotesList').subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.notes = res.data.data;
+      },
+      error: (err: Error) => {
+        console.log(err);
+      }
+    });
   }
-
-  notes : object[] = [
-    {
-      id: 1,
-      title: 'Grocery List',
-      description: 'Buy milk, eggs, bread, and coffee.'
-    },
-    {
-      id: 2,
-      title: 'Meeting Notes',
-      description: 'Discuss project milestones and deadlines with the team.'
-    },
-    {
-      id: 3,
-      title: 'Book Recommendations',
-      description: 'Read "Atomic Habits" by James Clear and "Sapiens" by Yuval Noah Harari.'
-    },
-    {
-      id: 4,
-      title: 'Workout Plan',
-      description: 'Monday: Cardio, Wednesday: Strength training, Friday: Yoga.'
-    },
-    {
-      id: 5,
-      title: 'Travel Itinerary',
-      description: 'Visit Tokyo Tower, Shibuya Crossing, and Meiji Shrine.'
-    },
-    {
-      id: 6,
-      title: 'Movie List',
-      description: 'Watch "The Shawshank Redemption," "The Godfather," and "The Dark Knight."'
-    },
-    {
-      id: 7,
-      title: 'Recipe Ideas',
-      description: 'Try making homemade pizza, pasta carbonara, and chocolate lava cake.'
-    },
-    {
-      id: 8,
-      title: 'Gift Suggestions',
-      description: 'Buy a watch, perfume, and flowers for your loved ones.'
-    }
-  ];
   
 
 }
